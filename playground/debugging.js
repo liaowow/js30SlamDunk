ee4f4fa0ceb21fa02b6de1ede5d158c7b292419f
Annie Liao <liaowow@gmail.com>
2020-12-02 09:42:40 -0500

    add debugging tools walkthrough

---
+++ playground/debugging.js
@@ -0,0 +1,25 @@
+const people = [
+  { name: 'Wes', cool: true, country: 'Canada' },
+  { name: 'Scott', cool: true, country: 'Merica' },
+  { name: 'Snickers', cool: false, country: 'Dog Country' },
+];
+
+people.forEach((person) => {
+  console.log(person.name);
+});
+
+// Console Methods
+
+// Callstack, stack trace
+
+// Grabbing Elements
+
+// Breakpoints
+
+// Scope
+
+// Network Requests
+
+// Break On Attribute
+
+// Some Setup Code
